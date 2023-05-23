e004756bc7835582b13c0bceb198a9d2a89d6c43
Anne Garvey <amgarvey15@gmail.com>
2023-05-23 14:36:27 +0900

    solved conflic

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,6 +1,5 @@
 class PostPolicy < ApplicationPolicy
 
-
   def show?
     true
   end
@@ -10,7 +9,7 @@ class PostPolicy < ApplicationPolicy
   end
 
   def create?
-   true
+    true
   end
 
   def update?
@@ -23,10 +22,6 @@ class PostPolicy < ApplicationPolicy
 
   def destroy?
     record.user == user
-  end
-
-  def create?
-    true
   end
 
   class Scope < Scope
