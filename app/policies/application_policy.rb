f7e38a35be10d705914a6d11ca7e1ee5cafc097a
Anne Garvey <amgarvey15@gmail.com>
2023-05-23 13:18:09 +0900

    half done Pundit and completed views

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -17,7 +17,7 @@ class ApplicationPolicy
   end
 
   def create?
-    true
+    false
   end
 
   def new?
@@ -25,7 +25,7 @@ class ApplicationPolicy
   end
 
   def update?
-    record.user == user
+    false
   end
 
   def edit?
@@ -33,7 +33,7 @@ class ApplicationPolicy
   end
 
   def destroy?
-    record.user == user
+    false
   end
 
   class Scope
---
+++ app/policies/post_policy.rb
@@ -0,0 +1,14 @@
+class PostPolicy < ApplicationPolicy
+
+  def create?
+    record.user == user
+  end
+
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+
+  end
+end
