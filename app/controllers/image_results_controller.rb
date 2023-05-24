85e742a883657b168ee356c7ce9c07c213120123
Cai Dixon <city.art.girl@gmail.com>
2023-05-24 12:28:33 +0900

    working on the api stuff stull

--- app/controllers/image_results_controller.rb
+++ app/controllers/image_results_controller.rb
@@ -16,8 +16,8 @@ class ImageResultsController < ApplicationController
 
   def create
     @result = ImageResult.new
-    ### here is where we will call the method to run the API
     @result.user = current_user
+    verifi(@result) if @result.photo.attached?
     authorize @image_results
     if @result.save
       redirect_to image_results_path(@result)
@@ -26,23 +26,25 @@ class ImageResultsController < ApplicationController
     end
   end
 
-
-  def verifi()
+  def verifi(result)
     uri = URI('https://api.sightengine.com/1.0/check.json')
-
     params = {
-      'url' => '',
-      'models' => 'nudity.sexual_activity,nudity.sexual_display,nudity.erotica,wad,offensive',
-      'api_user' => '{api_user}',
-      'api_secret' => '{api_secret}'
+      'url' => 'https://wallpapercave.com/wp/aYLVhao.jpg',
+      'models' => 'nudity-2.0,wad,offensive,gore',
+      'api_user' => '1608682898',
+      'api_secret' => 'PmEbv8uuWJUwtGinJATZ'
     }
-
     uri.query = URI.encode_www_form(params)
     response = Net::HTTP.get_response(uri)
 
     output = JSON.parse(response.body)
-
   end
+
+  # Cloudinary::Utils.cloudinary_url(result.photo.key)
+
+  # nudity.sexual_activity,nudity.sexual_display,nudity.erotica
+
+
   #### not needed for friday demo
 
   def destroy
