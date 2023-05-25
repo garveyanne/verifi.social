3a27176675119f3d39781a9e5467121d5c9ff827
Taylor Ellis <tayllorellis@gmail.com>
2023-05-25 16:52:09 +0900

    graph for results page

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import Chart from 'stimulus-chartjs'
+application.register('chart', Chart)
