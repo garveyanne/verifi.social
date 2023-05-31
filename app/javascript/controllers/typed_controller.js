d3005872c91b093d0a94f6d235284f38ac95fe71
Taylor Ellis <tayllorellis@gmail.com>
2023-05-31 13:42:17 +0900

    finished

--- app/javascript/controllers/typed_controller.js
+++ app/javascript/controllers/typed_controller.js
@@ -5,13 +5,14 @@ import Typed from 'typed.js';
 export default class extends Controller {
   static targets=["test"]
   connect() {
-    console.log("poo")
+    console.log("heyy")
   }
   display(){
     this.testTarget.style.display="flex"
     const typed = new Typed('.text', {
-      strings: ['Breaking down your photo ...', '&amp; Checking photo for high risk levels..', ' Building data...', 'Flagging areas with high risk...','Cleaning up...'],
-      typeSpeed: 80,
+      strings: ['Retrieving your photo from our database...^1500','Breaking down your photo ...^1500', ' Checking each photos risk levels....^1500', ' Building data...^1500', 'Comparing your data to our algorithm...^1500','Flagging areas with high risk....','Cleaning up...^1500'],
+      typeSpeed: 100,
+      backSpeed: 40,
     });
   }
 }
