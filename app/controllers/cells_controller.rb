f72f682a39aae6da0190a1e6f0907525c77bf370
Cai Dixon <city.art.girl@gmail.com>
2023-06-01 15:00:00 +0900

    testing things and heroku is great

--- app/controllers/cells_controller.rb
+++ app/controllers/cells_controller.rb
@@ -2,6 +2,7 @@ require 'open-uri'
 
 class CellsController < ApplicationController
   before_action :verify_authenticity_token, only: [:create]
+  skip_after_action :verify_authorized, only: [:create]
   def index
     @cells = policy_scope(Cell)
   end
@@ -26,19 +27,18 @@ class CellsController < ApplicationController
     cell_height = result.height / grid_size
     # itterate over the image grid cells
     # set the x and y axis points (top left corner of the cell)
-    (0...grid_size).each do |row|
-      (0...grid_size).each do |col|
+    cells = (0...grid_size).map do |row|
+      (0...grid_size).map do |col|
         x = col * cell_width
         y = row * cell_height
         # build the URL to request a cropped portion of that image
         url = "https://res.cloudinary.com/#{ENV['CLOUDINARY_NAME']}/image/upload/c_crop,h_#{cell_height},w_#{cell_width},x_#{x},y_#{y}/v1/#{Rails.env}/#{result.photo.key}"
         # verifi(result) is done before creation on the model
         # create cells with all info needed
-        cell = Cell.new(x_coor: x, y_coor: y, photo_url: url, image_result: result, row: row, col: col)
-        authorize cell
-        cell.save if cell.valid?
+        {x_coor: x, y_coor: y, photo_url: url, image_result_id: result.id, row: row, col: col}
       end
     end
+    Cell.create(cells.flatten)
   end
 
   ##### MOVED TO MODEL TO BE DONE BEFORE CREATE #####
