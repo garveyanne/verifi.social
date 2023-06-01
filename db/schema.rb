efcd4d4022eb462418cb816d79e3904172f89054
Cai Dixon <128205478+c8lindxson@users.noreply.github.com>
2023-06-01 14:06:51 +0900

    Revert "Grid rep"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_01_044316) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_29_070923) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,7 +59,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_01_044316) do
     t.string "profanity_intensity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["image_result_id", "col", "row"], name: "index_cells_on_image_result_id_and_col_and_row", unique: true
     t.index ["image_result_id"], name: "index_cells_on_image_result_id"
   end
 
