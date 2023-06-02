4dfcd7f2afd84974caa76c05e3ff9f097d187eda
Cai Dixon <city.art.girl@gmail.com>
2023-06-02 17:03:43 +0900

    changed key

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,6 +1,6 @@
 Cloudinary.config do |config|
-  config.cloud_name = 'di4ap7ugr'
-  config.api_key = '151345384988962'
-  config.api_secret = 'BjV4ewVvM8TcaX-GKU7iQ_23TNo'
+  config.cloud_name = 'dvo1h6tp2'
+  config.api_key = '263292251629499'
+  config.api_secret = 'krvGQUxLqY8eGX4bjwuZo5hfA8U'
   # Additional configuration options can be set here
 end
